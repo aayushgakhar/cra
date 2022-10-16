12eaf520ba7822a5a6db24c146f70d21a75b5577
Aayush Gakhar <aayush20006@iiitd.ac.in>
2022-10-16 15:50:30 +0530

    f

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -37,17 +37,39 @@ const Home = () => {
         </Link>
       </div>
       <div className=''>
-        <Link
-          to='work'
+        <a
+          href='https://drive.google.com/drive/folders/13HcrwHrLQaYD21GnqePtUKqAcj-pgwQK?usp=sharing'
           smooth={true}
           duration={500}
           className='w-fit text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-gradient-to-r hover:from-blue-800 hover:to-indigo-800 hover:border-blue-900'
         >
-          View Work
+          Resume
           <span className='group-hover:rotate-90 duration-300'>
             <HiArrowNarrowRight className='ml-3' />
           </span>
-        </Link>
+          <span>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              class='octicon arrow-symbol-mktg'
+              width='16'
+              height='16'
+              viewBox='0 0 16 16'
+              fill='none'
+            >
+              <path
+                fill='currentColor'
+                d='M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z'
+              ></path>
+              <path
+                class='octicon-chevrow-stem'
+                stroke='currentColor'
+                d='M1.75 8H11'
+                stroke-width='1.5'
+                stroke-linecap='round'
+              ></path>
+            </svg>
+          </span>
+        </a>
       </div>
     </section>
 
@@ -56,3 +78,4 @@ const Home = () => {
 };
 
 export default Home;
+
